7079e1b4473a4b98a45c873fa86ba701ffabb742
Kenneth David Leonel Triana <60140555+kennethLeonel@users.noreply.github.com>
2022-08-09 10:39:44 -0500

    parte funcional del programa

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,13 +1,76 @@
 
-    let arreglo =[];
+let arreglo =[];
 
 function encriptar() {
     let texto = document.getElementsByClassName("encriptador")[0].value;
-    arreglo.push(texto);
+    texto = texto.toLowerCase(); 
+    var concatenacion = "";
+    texto.split("").forEach(function (letra){
+        switch (letra) {
+            case "a":
+            concatenacion = concatenacion + "ai";
+            break;
+
+            case "e":
+            concatenacion = concatenacion + "enter";
+            break;
+
+            case "i":
+            concatenacion = concatenacion + "imes";
+            break;
+
+            case "o":
+            concatenacion = concatenacion + "ober";
+            break;
+
+            case "u":
+            concatenacion = concatenacion + "ufat";
+            break;
+
+            default:
+            concatenacion = concatenacion + letra;
+            
+            break;
+
+        }
+    }); 
+    arreglo.push(concatenacion);
     document.getElementById("historial").value = arreglo;
     // var historialMensajes = document.getElementsByClassName("historial");
     // historialMensajes.setAttribute("value",arreglo);
     document.getElementById("form1").reset();
+}
+
+function desencriptar(){
+    let texto = document.getElementsByClassName("encriptador")[0].value;
+    texto = texto.toLowerCase(); 
+    var concatenacion = "";
+    divi = texto.split("");
+    for (let i = 0; i < divi.length; i++) {
+        if (divi[i] == "o") {
+            concatenacion = concatenacion + divi[i];
+            i = i + 3;
+        }else if (divi[i] == "u") {
+            concatenacion = concatenacion + divi[i];
+            i = i + 3;
+        }else if (divi[i] == "i") {
+            concatenacion = concatenacion + divi[i];
+            i = i + 3;
+        }else if (divi[i] == "e") {
+            concatenacion = concatenacion + divi[i];
+            i = i + 4;
+        }else if (divi[i] == "a") {
+            concatenacion = concatenacion + divi[i];
+            i = i + 1;
+        }
+        else {
+            concatenacion = concatenacion + divi[i];
+        }
+    }
+    arreglo.push(concatenacion);
+    document.getElementById("historial").value = arreglo;
+    document.getElementById("form1").reset();
+
 }
 
 function copiar(){
