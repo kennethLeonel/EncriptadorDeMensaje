02c585157405c044ea36c82b4175e25e78352d49
Kenneth David Leonel Triana <60140555+kennethLeonel@users.noreply.github.com>
2022-08-10 08:49:37 -0500

    validaciones corregidas

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -3,103 +3,98 @@ let arreglo =[];
 limpiar();
 function encriptar() {
     let texto = document.getElementsByClassName("encriptador")[0].value;
-    texto = texto.toLowerCase(); 
-    var concatenacion = "";
-    texto.split("").forEach(function (letra){
-        switch (letra) {
-            case "a":
-            concatenacion = concatenacion + "ai";
-            break;
-
-            case "e":
-            concatenacion = concatenacion + "enter";
-            break;
-
-            case "i":
-            concatenacion = concatenacion + "imes";
-            break;
-
-            case "o":
-            concatenacion = concatenacion + "ober";
-            break;
-
-            case "u":
-            concatenacion = concatenacion + "ufat";
-            break;
-
-            default:
-            concatenacion = concatenacion + letra;
-            
-            break;
-
-        }
-    }); 
-    if (concatenacion !=" "){
-        let imagen = document.getElementById("imgMensaje");
-        imagen.src = "../resources/img/detectiveEncriptado.png"; 
-        imagen.style.width = "200px";
-        let titulo = document.getElementById("histoH3");
-        titulo.innerHTML = "Historial de mensajes";
+    if ((texto == "") || (texto.trim().length == 0)) {
+        alert("Ingrese un mensaje");
+        resetearFormulario();
+    } else {
+        texto = texto.toLowerCase(); 
+        var concatenacion = "";
+        texto.split("").forEach(function (letra){
+            switch (letra) {
+                case "a":
+                concatenacion = concatenacion + "ai";
+                break;
+    
+                case "e":
+                concatenacion = concatenacion + "enter";
+                break;
+    
+                case "i":
+                concatenacion = concatenacion + "imes";
+                break;
+    
+                case "o":
+                concatenacion = concatenacion + "ober";
+                break;
+    
+                case "u":
+                concatenacion = concatenacion + "ufat";
+                break;
+    
+                default:
+                concatenacion = concatenacion + letra;
+                
+                break;
+    
+            }
+        });
         arreglo.push(concatenacion);
+        cambiarImagen();
+        resetearFormulario();
     }
-    document.getElementById("historial").value = arreglo;
-    // var historialMensajes = document.getElementsByClassName("historial");
-    // historialMensajes.setAttribute("value",arreglo);
-    document.getElementById("form1").reset();
+ 
+
+    
 }
 
 function desencriptar(){
     let texto = document.getElementsByClassName("encriptador")[0].value;
-    texto = texto.toLowerCase(); 
-    var concatenacion = "";
-    divi = texto.split("");
-    for (let i = 0; i < divi.length; i++) {
-        if (divi[i] == "o") {
-            concatenacion = concatenacion + divi[i];
-            // i = i + 3;
-            if ((divi[i+1] == "b") && (divi[i+2] == "e") && (divi[i+3] == "r")) {
+    if ((texto == "") || (texto.trim().length == 0)) {
+        alert("Ingrese un mensaje");
+        resetearFormulario();
+    }else{
+        texto = texto.toLowerCase(); 
+        var concatenacion = "";
+        divi = texto.split("");
+        for (let i = 0; i < divi.length; i++) {
+            if (divi[i] == "o") {
+                concatenacion = concatenacion + divi[i];
+                // i = i + 3;
+                if ((divi[i+1] == "b") && (divi[i+2] == "e") && (divi[i+3] == "r")) {
+                    i = i + 3;
+                }
+            }else if (divi[i] == "u") {
+                
+                concatenacion = concatenacion + divi[i];
+                if((divi[i+1] == "f") && (divi[i+2] == "a") && (divi[i+3] == "t")){
                 i = i + 3;
+                }
+            }else if (divi[i] == "i") {
+                concatenacion = concatenacion + divi[i];
+                if((divi[i+1] == "m") && (divi[i+2] == "e") && (divi[i+3] == "s")){
+                i = i + 3;
+                }
+            }else if (divi[i] == "e") {
+                concatenacion = concatenacion + divi[i];
+                if ((divi[i+1] == "n") && (divi[i+2] == "t") && (divi[i+3] == "e") && (divi[i+4] == "r")) {
+                    i = i + 4;
+                }
+             
+            }else if (divi[i] == "a") {
+                concatenacion = concatenacion + divi[i];
+                if ((divi[i+1] == "i")) {
+                    i = i + 1;
+                }
             }
-        }else if (divi[i] == "u") {
-            
-            concatenacion = concatenacion + divi[i];
-            if((divi[i+1] == "f") && (divi[i+2] == "a") && (divi[i+3] == "t")){
-            i = i + 3;
-            }
-        }else if (divi[i] == "i") {
-            concatenacion = concatenacion + divi[i];
-            if((divi[i+1] == "m") && (divi[i+2] == "e") && (divi[i+3] == "s")){
-            i = i + 3;
-            }
-        }else if (divi[i] == "e") {
-            concatenacion = concatenacion + divi[i];
-            if ((divi[i+1] == "n") && (divi[i+2] == "t") && (divi[i+3] == "e") && (divi[i+4] == "r")) {
-                i = i + 4;
-            }
-         
-        }else if (divi[i] == "a") {
-            concatenacion = concatenacion + divi[i];
-            if ((divi[i+1] == "i")) {
-                i = i + 1;
+            else {
+                concatenacion = concatenacion + divi[i];
             }
         }
-        else {
-            concatenacion = concatenacion + divi[i];
-        }
-    }
-    if (concatenacion !=" "){
         arreglo.push(concatenacion);
-        let imagen = document.getElementById("imgMensaje");
-        imagen.src = "../resources/img/detectiveEncriptado.png";
-        imagen.style.width = "200px";
-        let titulo = document.getElementById("histoH3");
-        titulo.innerHTML = "Historial de mensajes";
+        cambiarImagen();
+        resetearFormulario();
+
     }
-
-    
-    document.getElementById("historial").value = arreglo;
-    document.getElementById("form1").reset();
-
 }
 
 function copiar(){
@@ -130,6 +125,19 @@ function limpiar(){
       titulo.innerHTML = "Sin Mensajes";
       arreglo = [];
 
+}
+
+function resetearFormulario(){
+    document.getElementById("historial").value = arreglo;
+    document.getElementById("form1").reset();
+}
+
+function cambiarImagen(){
+    let imagen = document.getElementById("imgMensaje");
+    imagen.src = "../resources/img/detectiveEncriptado.png"; 
+    imagen.style.width = "200px";
+    let titulo = document.getElementById("histoH3");
+    titulo.innerHTML = "Historial de mensajes";
 }
     // var encriptado = CryptoJS.AES.encrypt(texto, "secret key 123");
     // return encriptado.toString();
