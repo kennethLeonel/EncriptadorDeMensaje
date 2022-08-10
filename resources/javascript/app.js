09133145526e8b41fbc012c65464135c1eb3add7
Kenneth David Leonel Triana <60140555+kennethLeonel@users.noreply.github.com>
2022-08-09 22:27:20 -0500

    a ver

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -36,7 +36,7 @@ function encriptar() {
     }); 
     if (concatenacion !=" "){
         let imagen = document.getElementById("imgMensaje");
-        imagen.src = "../resources/img/detectiveEncriptado.png"; 
+        imagen.src = ".../resources/img/detectiveEncriptado.png"; 
         imagen.style.width = "200px";
         let titulo = document.getElementById("histoH3");
         titulo.innerHTML = "Historial de mensajes";
@@ -90,7 +90,7 @@ function desencriptar(){
     if (concatenacion !=" "){
         arreglo.push(concatenacion);
         let imagen = document.getElementById("imgMensaje");
-        imagen.src = "../resources/img/detectiveEncriptado.png";
+        imagen.src = ".../resources/img/detectiveEncriptado.png";
         imagen.style.width = "200px";
         let titulo = document.getElementById("histoH3");
         titulo.innerHTML = "Historial de mensajes";
@@ -124,7 +124,7 @@ function copiar(){
 function limpiar(){
       document.getElementById("historial").value = "";
       let imagen = document.getElementById("imgMensaje");
-      imagen.src = "../resources/img/imgMensaje.svg"; 
+      imagen.src = ".../resources/img/imgMensaje.svg"; 
       imagen.style.width = "200px";
       let titulo = document.getElementById("histoH3");
       titulo.innerHTML = "Sin Mensajes";
